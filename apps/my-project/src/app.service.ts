42b45badc9f7f06637facf684fdd4d566468e2bc
Elvis <elvis.e.amancio@gmail.com>
2024-12-12 04:54:15 -0300

    'my-project' modificado para teste de deploy

--- apps/my-project/src/app.service.ts
+++ apps/my-project/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log('[my-project modificado para teste de deploy]');
     return 'Hello World [my-project]!';
   }
 }
