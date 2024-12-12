8f68b0c2ae62180cacbb74b4928aa06b756b9f0c
Elvis <elvis.e.amancio@gmail.com>
2024-12-12 04:05:59 -0300

    modificação teste 'my-app'

--- apps/my-app/src/my-app.service.ts
+++ apps/my-app/src/my-app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class MyAppService {
   getHello(): string {
+    console.log('Hello World [my-app]!');
+
     return 'Hello World [my-app]!';
   }
 }
