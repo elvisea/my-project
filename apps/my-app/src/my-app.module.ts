63ca35efdda139cf9ac67bcbf5c1483baa23b61c
Elvis <elvis.e.amancio@gmail.com>
2024-12-13 00:43:48 -0300

    adicionado conexão da aplicação com banco de dados

--- apps/my-app/src/my-app.module.ts
+++ apps/my-app/src/my-app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
-import { MyAppController } from './my-app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { MyAppService } from './my-app.service';
+import { MyAppController } from './my-app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local', '.env.development', '.env'],
+      cache: false,
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [],
+      }),
+    }),
+  ],
   controllers: [MyAppController],
   providers: [MyAppService],
 })
--- apps/my-project/src/app.module.ts
+++ apps/my-project/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local', '.env.development', '.env'],
+      cache: false,
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [],
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
