bf74137a9bbf47ca8fb4c8ad192ef59350e772df
Elvis <elvis.e.amancio@gmail.com>
2024-12-13 00:01:44 -0300

    adicionando mensagem de boa noite

--- apps/my-project/src/app.service.ts
+++ apps/my-project/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World [my-project]!';
+    return '🌙✨ Boa noite! Que seus sonhos sejam tão lindos quanto você! 😴💫 Que o descanso te traga{\n}paz e energia para um novo dia cheio de conquistas! 🌟🌙 Durma bem e acorde com o coração leve! 💖🌜 🛏️😌 Boa noite!';
   }
 }
