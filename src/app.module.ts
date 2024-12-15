f9d5ce95f1cd9a158ca67154098ddab52b8efcce
Elvis <elvis.e.amancio@gmail.com>
2024-12-15 18:18:00 -0300

    adicionado configuração do banco de dados com typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerOptions } from 'typeorm';
 
 @Module({
   imports: [
@@ -11,6 +13,33 @@ import { AppController } from './app.controller';
       envFilePath: ['.env.development.local', '.env.development', '.env'],
       cache: false,
       expandVariables: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const logging: LoggerOptions =
+          configService.get('NODE_ENV') === 'development'
+            ? ['query', 'error', 'schema', 'warn']
+            : [];
+
+        console.log(`📅 Data: ${new Date().toLocaleString()}`);
+        console.log(
+          `Environment: ${configService.get('NODE_ENV') === 'development'}`,
+        );
+
+        return {
+          type: 'postgres',
+          host: configService.get('POSTGRES_HOST'),
+          port: configService.get('POSTGRES_PORT'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DB'),
+          entities: [],
+          synchronize: true,
+          logging: logging,
+        };
+      },
     }),
   ],
   controllers: [AppController],
