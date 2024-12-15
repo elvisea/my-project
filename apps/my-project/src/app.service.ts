af91f7595208e772a245ba7bef646bb94b1c7511
Elvis <elvis.e.amancio@gmail.com>
2024-12-14 21:54:14 -0300

    change message

--- apps/my-project/src/app.service.ts
+++ apps/my-project/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World [MyProject]';
+    return 'Hello World [My Project]';
   }
 }
